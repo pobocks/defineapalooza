0f70c40dacae2b517af1ea65c94c13368d7f6379
Dave Mayo <dave_mayo@harvard.edu>
2017-11-13 22:26:45 -0500

    Changed to "new_episodes" record mode due to weird "false update" behavior.

--- tests/common.py
+++ tests/common.py
@@ -2,4 +2,4 @@ from vcr import VCR
 import defineapalooza
 
 app = defineapalooza.app.test_client()
-betamax = VCR(func_path_generator = lambda test: "tests/fixtures/cassettes/{}.yaml".format(test.__module__))
+betamax = VCR(record_mode="new_episodes", func_path_generator = lambda test: "tests/fixtures/cassettes/{}--{}.yaml".format(test.__module__, test.__name__))
